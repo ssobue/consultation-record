485e63a0817b2f152ef9522dd6f3e56db20bd141
SOBUE Sho <sho@sobue.dev>
2021-04-30 21:35:03 +0900

    Init Spring Boot

---
+++ src/main/java/dev/sobue/consultation/ConsultationRecordApplication.java
@@ -0,0 +1,13 @@
+package dev.sobue.consultation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultationRecordApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsultationRecordApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/sobue/consultation/ConsultationRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sobue.consultation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultationRecordApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
